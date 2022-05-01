cc1497402d9723c0eee29391c7290777a313d99d
Clebson Moura <clebson.sh@gmail.com>
2022-05-01 20:34:52 -0300

    adicionar veiculos

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 "use strict";
 (function () {
     const $ = (query) => document.querySelector(query);
+    function patio() {
+        function ler() { }
+        function adicionar(veiculo) {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+        <td>${veiculo.nome}</td>
+        <td>${veiculo.placa}</td>
+        <td>${veiculo.entrada}</td>
+        <td>
+          <button class="delete" data-placa="${veiculo.placa}">X</button>
+        </td>
+      `;
+            $('#patio').appendChild(row);
+        }
+        function remover() { }
+        function salvar() { }
+        function render() { }
+        return { ler, adicionar, remover, salvar, render };
+    }
     $('#cadastrar').addEventListener('click', () => {
         const nome = $('#nome').value;
         const placa = $('#placa').value;
@@ -8,5 +27,6 @@
             alert("Os campos nome e placa são obrigatórios");
             return;
         }
+        patio().adicionar({ nome, placa, entrada: new Date() });
     });
 })();
--- script.ts
+++ script.ts
@@ -1,5 +1,38 @@
+interface IVeiculo {
+  nome: string
+  placa: string
+  entrada: Date
+}
+
 (function() {
   const $ = (query: string) => document.querySelector(query) as HTMLInputElement
+
+  function patio() {
+    function ler() {}
+
+    function adicionar(veiculo: IVeiculo) {
+      const row = document.createElement("tr")
+
+      row.innerHTML= `
+        <td>${veiculo.nome}</td>
+        <td>${veiculo.placa}</td>
+        <td>${veiculo.entrada}</td>
+        <td>
+          <button class="delete" data-placa="${veiculo.placa}">X</button>
+        </td>
+      `
+
+      $('#patio').appendChild(row)
+    }
+
+    function remover() {}
+
+    function salvar() {}
+
+    function render() {}
+
+    return { ler, adicionar, remover, salvar, render }
+  }
 
   $('#cadastrar').addEventListener('click', () => {
     const nome = $('#nome').value
@@ -9,5 +42,7 @@
       alert("Os campos nome e placa são obrigatórios")
       return
     }
+
+    patio().adicionar({ nome, placa, entrada: new Date() })
   })
 })()
